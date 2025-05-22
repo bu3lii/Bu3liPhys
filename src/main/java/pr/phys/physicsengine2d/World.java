2a1448d15d3005d77869b5cc809c37df7befa1cd
bu3li <hussain.n.k07@gmail.com>
2025-05-22 22:49:32 +0300

    feat(physics): Create World class which brings everything together
    - Manages physics objects and global forces
    - Initialize downwards gravity (0,9.8)
    - Object management with safe add/get operations
    - Add physics step simulation with proper time handling
    - Include gravity force application to all objects
    - More OOP rahhhhhhh

---
+++ src/main/java/pr/phys/physicsengine2d/World.java
@@ -0,0 +1,50 @@
+package pr.phys.physicsengine2d;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class World {
+private final List<PhysicsObject> objects;
+private Vector2D gravity;
+
+public World(){
+    objects = new ArrayList<>();
+    gravity = new Vector2D(0,9.8);
+}
+
+public void addObject(PhysicsObject obj){
+    if(obj!=null) {
+        objects.add(obj);
+    }
+}
+
+public void step(double dt){
+    if(dt<0){
+        throw new IllegalArgumentException("Time step cannot be less than zero");
+    }
+    for(PhysicsObject obj : objects){
+        obj.applyForce(gravity.multiplyScalar(obj.getMass()));
+        obj.update(dt);
+    }
+}
+
+public List<PhysicsObject> getObjects(){
+    return Collections.unmodifiableList(objects);
+}
+
+public void setGravity(Vector2D g){
+    if(gravity==null){
+        throw new IllegalArgumentException("Gravity Cannot be Null");
+    }
+    gravity = g;
+}
+
+public Vector2D getGravity(){
+    return new Vector2D(gravity.x, gravity.y);
+}
+
+
+}
